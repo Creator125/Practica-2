63482d08d041bda3b5164d0ce4779bf063e2174c
Creator125 OCA <85368990+Creator125@users.noreply.github.com>
2021-06-10 19:17:09 +0000

    prueba

--- main.cs
+++ main.cs
@@ -3,6 +3,8 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
     double var1 = 19.20;
-    Console.WriteLine (var1);
+    double var2 = 65.4;
+    double suma = var1 + var2;
+    Console.WriteLine (suma);
   }
 }
