f04fb2e17b4ebba6c7ef206b24a990819182c495
Creator125 OCA <85368990+Creator125@users.noreply.github.com>
2021-06-10 19:54:40 +0000

    Prueba 3

--- main.cs
+++ main.cs
@@ -4,7 +4,6 @@ class MainClass {
   public static void Main (string[] args) {
     double var1 = 19.20;
     double var2 = 65.4;
-    double suma = var1 + var2;
-    Console.WriteLine (suma);
+    Console.WriteLine (var1 + var2);
   }
 }
